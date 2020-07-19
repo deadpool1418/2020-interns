6fa7e9bb76c7a7cd9480d98dd33af434bad871bb
Cypher <vachaudhari26@gmail.com>
2020-07-19 10:51:29 +0530

    Task 2

---
+++ task2.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 18 10:04:49 2020
+
+@author: vijay
+"""
+
+import json
+from datetime import timedelta, date
+import matplotlib.pyplot as plt
+
+
+def daterange(start_date, end_date):
+    for n in range(int((end_date - start_date).days)):
+        yield start_date + timedelta(n)
+
+
+with open('data.json') as f:
+    perf = json.load(f)
+rates=perf['rates']
+print("Enter currency symbol1 (eg: INR for Indian Rupees)")
+sym1=input()
+print("Enter currency symbol2 (eg: INR for Indian Rupees)")
+sym2=input()
+print("Enter start date (YYYY-MM-DD)")
+y,m,d=map(int,input().split("-"))
+start_date = date(y,m,d)
+print("Enter end date (YYYY-MM-DD)")
+y,m,d=map(int,input().split("-"))
+end_date = date(y,m,d)
+
+date=[]
+l1=[]
+l2=[]
+for single_date in daterange(start_date, end_date):
+    try:
+        l1.append(rates[single_date.strftime("%Y-%m-%d")][sym1])
+        l2.append(rates[single_date.strftime("%Y-%m-%d")][sym2])
+        date.append(single_date.strftime("%Y-%m-%d"))
+    except:
+       None
+plt.figure(figsize=(30,20)) 
+plt.subplot(1,2,1)       
+plt.plot(date,l1,label=sym1)
+plt.xlabel("Dates in month of Jan 2019")
+plt.xticks(rotation=90)
+plt.ylabel(sym1+" exchange rates")
+plt.legend(loc="upper left")
+plt.subplot(1,2,2)
+plt.plot(date,l2,label=sym2)
+plt.xlabel("Dates in month of Jan 2019")
+plt.xticks(rotation=90)
+plt.ylabel(sym2+" exchange rates")
+plt.legend(loc="upper left")
+plt.show()  
