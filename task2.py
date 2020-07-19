49f95b903670d1a9469d3718056f858587b31c47
Cypher <vachaudhari26@gmail.com>
2020-07-19 19:08:52 +0530

    Task 2 Updation

--- task2.py
+++ task2.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
+
+2019-01-01
+2019-01-31
+
 Created on Sat Jul 18 10:04:49 2020
 
 @author: vijay
@@ -8,7 +12,6 @@ Created on Sat Jul 18 10:04:49 2020
 
 import json
 from datetime import timedelta, date
-import matplotlib.pyplot as plt
 
 
 def daterange(start_date, end_date):
@@ -19,38 +22,45 @@ def daterange(start_date, end_date):
 with open('data.json') as f:
     perf = json.load(f)
 rates=perf['rates']
-print("Enter currency symbol1 (eg: INR for Indian Rupees)")
-sym1=input()
-print("Enter currency symbol2 (eg: INR for Indian Rupees)")
-sym2=input()
+print("Enter symbol 1")
+sym=input()
+sym2=input("Enter symbol 2")
 print("Enter start date (YYYY-MM-DD)")
 y,m,d=map(int,input().split("-"))
 start_date = date(y,m,d)
-print("Enter end date (YYYY-MM-DD)")
+print("Enter End date (YYYY-MM-DD)")
 y,m,d=map(int,input().split("-"))
 end_date = date(y,m,d)
-
 date=[]
 l1=[]
 l2=[]
 for single_date in daterange(start_date, end_date):
     try:
-        l1.append(rates[single_date.strftime("%Y-%m-%d")][sym1])
+        l1.append(rates[single_date.strftime("%Y-%m-%d")][sym])
         l2.append(rates[single_date.strftime("%Y-%m-%d")][sym2])
         date.append(single_date.strftime("%Y-%m-%d"))
     except:
        None
-plt.figure(figsize=(30,20)) 
-plt.subplot(1,2,1)       
-plt.plot(date,l1,label=sym1)
-plt.xlabel("Dates in month of Jan 2019")
-plt.xticks(rotation=90)
-plt.ylabel(sym1+" exchange rates")
-plt.legend(loc="upper left")
-plt.subplot(1,2,2)
-plt.plot(date,l2,label=sym2)
-plt.xlabel("Dates in month of Jan 2019")
-plt.xticks(rotation=90)
-plt.ylabel(sym2+" exchange rates")
-plt.legend(loc="upper left")
-plt.show()  
+
+mx,mn=max(l1),min(l1)
+mx1,mn1=max(l2),min(l2)
+f=open("graph.svg","w")
+f.write('''<svg width="1000" height="1000" xmlns="http://www.w3.org/2000/svg">\n''')
+f.write('''<rect width="100%" height="100%" fill="white" />\n''')
+f.write("<text x=\"70\" y=\"15\" fill=\"red\">"+sym+"Exchange rates (Base EUR)"+"</text>\n")
+for i in range(len(l1)):
+    a=350-150*(l1[i]-mn)/(mx-mn)
+    b=100+i*10
+    f.write('''<circle cx='''+'''"'''+str(b)+'''"'''+''' cy='''+'''"'''+str(a)+'''"'''+''' r="3"/> \n''')
+    f.write("<text x=\"" + str(b-5) + "\" y=\"400\" fill=\"red\" style=\"font: 12px sans-serif;\" transform=\"rotate(90,"+str(b-5)+",400)\">"+date[i]+"</text>")
+f.write("</svg>")    
+f=open("graph1.svg","w")
+f.write('''<svg width="1000" height="1000" xmlns="http://www.w3.org/2000/svg">\n''')
+f.write('''<rect width="100%" height="100%" fill="white" />\n''')
+f.write("<text x=\"70\" y=\"15\" fill=\"red\">"+sym2+"Exchange rates (Base EUR)"+"</text>\n")
+for i in range(len(l2)):
+    a=350-150*(l2[i]-mn1)/(mx1-mn1)
+    b=100+i*10
+    f.write('''<circle cx='''+'''"'''+str(b)+'''"'''+''' cy='''+'''"'''+str(a)+'''"'''+''' r="3"/> \n''')
+    f.write("<text x=\"" + str(b-5) + "\" y=\"400\" fill=\"red\" style=\"font: 12px sans-serif;\" transform=\"rotate(90,"+str(b-5)+",400)\">"+date[i]+"</text>")
+f.write("</svg>")    
